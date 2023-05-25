3fc010ac2f91140a3a2891255e202107ee0a3a6f
oferk <ofer.karp@gmail.com>
2023-05-25 18:36:19 +0300

    doghub 71

--- src/main/java/org/hk/doghub/ui/views/app/users/create/UserCreationViewHeaderInfoTitleIcon.java
+++ src/main/java/org/hk/doghub/ui/views/app/users/create/UserCreationViewHeaderInfoTitleIcon.java
@@ -3,14 +3,14 @@ package org.hk.doghub.ui.views.app.users.create;
 import com.vaadin.flow.component.icon.Icon;
 import org.hk.doghub.security.AuthenticatedUser;
 
-import static com.vaadin.flow.component.icon.VaadinIcon.USERS;
+import static com.vaadin.flow.component.icon.VaadinIcon.USER;
 
 public class UserCreationViewHeaderInfoTitleIcon extends Icon {
 
     public static final String CLASS_NAME = UserCreationViewHeaderInfoTitle.CLASS_NAME + "-icon";
 
     public UserCreationViewHeaderInfoTitleIcon(AuthenticatedUser authenticatedUser, UserCreationService userCreationService) {
-        super(USERS);
+        super(USER);
         setSize("60px");
         addClassName(CLASS_NAME);
     }
