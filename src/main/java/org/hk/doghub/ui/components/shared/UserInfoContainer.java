74929ade689b17f045bf0fe6fbb6f231e3b0d6c3
oferk <ofer.karp@gmail.com>
2023-05-26 22:49:16 +0300

    doghub 89

--- src/main/java/org/hk/doghub/ui/components/shared/UserInfoContainer.java
+++ src/main/java/org/hk/doghub/ui/components/shared/UserInfoContainer.java
@@ -2,8 +2,11 @@ package org.hk.doghub.ui.components.shared;
 
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import org.hk.doghub.model.user.DogHubUser;
 import org.hk.doghub.security.AuthenticatedUser;
 import org.hk.doghub.ui.views.app.users.UsersDataProvider;
+
+import java.util.Optional;
 
 import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
 
@@ -11,18 +14,35 @@ public class UserInfoContainer extends VerticalLayout {
 
     public static final String CLASS_NAME = "user-info-container";
 
+    private final UsersDataProvider usersDataProvider;
+
     private final Label id;
+    private final Label name;
+    private final Label username;
 
     public UserInfoContainer(UsersDataProvider usersDataProvider, AuthenticatedUser authenticatedUser, UserCreationService userCreationService) {
+        this.usersDataProvider = usersDataProvider;
         addClassName(CLASS_NAME);
 
         setAlignItems(CENTER);
 
         id = new Label();
         add(id);
+
+        name = new Label();
+        add(name);
+
+        username = new Label();
+        add(username);
     }
 
     public void setUser(long userId) {
-        id.setText("User Id: " + userId);
+        Optional<DogHubUser> userOptional = usersDataProvider.findById(userId);
+        if(userOptional.isPresent()) {
+            id.setText("Id: " + userId);
+            DogHubUser user = userOptional.get();
+            name.setText("Name: " + user.getName());
+            username.setText("Username: " + user.getUsername());
+        }
     }
 }
