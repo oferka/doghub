82bb8989a87d7d354d180b91c1272153cf5e0d9a
oferk <ofer.karp@gmail.com>
2023-05-26 16:50:16 +0300

    doghub 84

--- src/main/java/org/hk/doghub/ui/views/app/users/create/UserCreationViewBody.java
+++ src/main/java/org/hk/doghub/ui/views/app/users/create/UserCreationViewBody.java
@@ -1,8 +1,8 @@
 package org.hk.doghub.ui.views.app.users.create;
 
-import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import org.hk.doghub.security.AuthenticatedUser;
+import org.hk.doghub.ui.components.shared.UserCreationContainer;
 import org.hk.doghub.ui.components.shared.UserCreationService;
 
 import static com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER;
@@ -11,12 +11,15 @@ public class UserCreationViewBody extends HorizontalLayout {
 
     public static final String CLASS_NAME = UserCreationView.CLASS_NAME + "-body";
 
+    private final UserCreationContainer userCreationContainer;
+
     public UserCreationViewBody(AuthenticatedUser authenticatedUser, UserCreationService userCreationService) {
         addClassName(CLASS_NAME);
 
         setWidthFull();
         setAlignItems(CENTER);
 
-        add(new Span("user creation body"));
+        userCreationContainer = new UserCreationContainer(userCreationService);
+        add(userCreationContainer);
     }
 }
