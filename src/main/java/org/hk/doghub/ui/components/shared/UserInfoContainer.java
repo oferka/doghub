533206fcb27fb848b7b6722eae769e2a657cc4d0
oferk <ofer.karp@gmail.com>
2023-05-28 13:56:20 +0300

    doghub 112

--- src/main/java/org/hk/doghub/ui/components/shared/UserInfoContainer.java
+++ src/main/java/org/hk/doghub/ui/components/shared/UserInfoContainer.java
@@ -36,7 +36,7 @@ public class UserInfoContainer extends VerticalLayout {
     private final TextField company;
     private final TextField mobileNumber;
     private final ComboBox<String> country;
-    private final TextField state;
+    private final ComboBox<String> state;
     private final TextField city;
     private final TextField streetName;
     private final IntegerField number;
@@ -109,10 +109,13 @@ public class UserInfoContainer extends VerticalLayout {
         country.setHelperText("Select or type your country");
         country.setPrefixComponent(MAP_MARKER.create());
 
-        state = new TextField("State");
+        state = new ComboBox<>("State");
         state.setClearButtonVisible(true);
-        state.setMinLength(2);
-        state.setMaxLength(64);
+        List<String> stateValues = asList("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
+                "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio",
+                "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming");
+        state.setItems(stateValues);
+        state.setHelperText("Select or type your state");
         state.setPrefixComponent(MAP_MARKER.create());
 
         city = new TextField("City");
