4a94d9f38cbc31258f0ce43f8ef124e0448beab4
oferk <ofer.karp@gmail.com>
2023-05-29 16:21:50 +0300

    doghub 129

--- src/main/java/org/hk/doghub/ui/components/shared/UsersRouterLink.java
+++ src/main/java/org/hk/doghub/ui/components/shared/UsersRouterLink.java
@@ -3,12 +3,19 @@ package org.hk.doghub.ui.components.shared;
 import com.vaadin.flow.router.RouterLink;
 import org.hk.doghub.ui.views.app.users.UsersView;
 
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+
 public class UsersRouterLink extends RouterLink {
 
     public static final String CLASS_NAME = "users-router-link";
 
+    private final UsersButton usersButton;
+
     public UsersRouterLink() {
-        super("Users", UsersView.class);
+        super(EMPTY, UsersView.class);
         addClassName(CLASS_NAME);
+
+        usersButton = new UsersButton();
+        add(usersButton);
     }
 }
