b1dc1fcede4ff3a655acc2aff83b0eb94f75a06f
oferk <ofer.karp@gmail.com>
2023-06-01 14:45:03 +0300

    doghub 187

--- src/main/java/org/hk/doghub/ui/components/shared/UserDateOfRegistrationField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/UserDateOfRegistrationField.java
@@ -4,8 +4,11 @@ import com.vaadin.flow.component.datetimepicker.DateTimePicker;
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.user.DogHubUser;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+
+import static java.text.MessageFormat.format;
 
 public class UserDateOfRegistrationField extends DateTimePicker {
 
@@ -17,13 +20,36 @@ public class UserDateOfRegistrationField extends DateTimePicker {
         addClassName(CLASS_NAME);
         setLabel(LABEL);
         setReadOnly(true);
+        addValueChangeListener(this::valueChanged);
+    }
+
+    private void valueChanged(ComponentValueChangeEvent<DateTimePicker, LocalDateTime> event) {
+        LocalDateTime value = event.getValue();
+        setValueWithValidation(value);
     }
 
     public void setValue(@NotNull DogHubUser user) {
-        setValue((user.getDateOfRegistration() != null)?user.getDateOfRegistration().toLocalDateTime() : null);
+        LocalDateTime value = (user.getDateOfRegistration() != null) ? user.getDateOfRegistration().toLocalDateTime() : null;
+        setValue(value);
+    }
+
+    private void setValueWithValidation(LocalDateTime value) {
+        if(isValidValue(value)) {
+            setValue(value);
+            setInvalid(false);
+            setErrorMessage(null);
+        }
+        else {
+            setInvalid(true);
+            setErrorMessage(format("Must be a valid date & time"));
+        }
     }
 
     public ZonedDateTime getValueAsZonedDateTime() {
         return ((getValue() != null) ? ZonedDateTime.of(getValue(), ZoneId.systemDefault()) : null);
+    }
+
+    private boolean isValidValue(LocalDateTime value) {
+        return(value != null && value.isBefore(LocalDateTime.now()));
     }
 }
