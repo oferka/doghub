1fddba4faf34a245b2821bc30e33f5d4aeeb658a
oferk <ofer.karp@gmail.com>
2023-06-01 14:38:53 +0300

    doghub 186

--- src/main/java/org/hk/doghub/ui/components/shared/UserDateOfBirthField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/UserDateOfBirthField.java
@@ -4,8 +4,11 @@ import com.vaadin.flow.component.datetimepicker.DateTimePicker;
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.user.DogHubUser;
 
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+
+import static java.text.MessageFormat.format;
 
 public class UserDateOfBirthField extends DateTimePicker {
 
@@ -16,13 +19,36 @@ public class UserDateOfBirthField extends DateTimePicker {
     public UserDateOfBirthField() {
         addClassName(CLASS_NAME);
         setLabel(LABEL);
+        addValueChangeListener(this::valueChanged);
+    }
+
+    private void valueChanged(ComponentValueChangeEvent<DateTimePicker, LocalDateTime> event) {
+        LocalDateTime value = event.getValue();
+        setValueWithValidation(value);
     }
 
     public void setValue(@NotNull DogHubUser user) {
-        setValue((user.getDateOfBirth() != null)?user.getDateOfBirth().toLocalDateTime() : null);
+        LocalDateTime value = (user.getDateOfBirth() != null) ? user.getDateOfBirth().toLocalDateTime() : null;
+        setValue(value);
+    }
+
+    private void setValueWithValidation(LocalDateTime value) {
+        if(isValidValue(value)) {
+            setValue(value);
+            setInvalid(false);
+            setErrorMessage(null);
+        }
+        else {
+            setInvalid(true);
+            setErrorMessage(format("Must be a valid date & time"));
+        }
     }
 
     public ZonedDateTime getValueAsZonedDateTime() {
         return ((getValue() != null) ? ZonedDateTime.of(getValue(), ZoneId.systemDefault()) : null);
+    }
+
+    private boolean isValidValue(LocalDateTime value) {
+        return(value != null && value.isBefore(LocalDateTime.now()));
     }
 }
