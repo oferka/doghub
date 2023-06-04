21d7a134e3f7a25d7a40d13271b2b89b3e97478f
oferk <ofer.karp@gmail.com>
2023-06-04 13:27:05 +0300

    doghub 216

---
+++ src/main/java/org/hk/doghub/ui/components/shared/NameField.java
@@ -0,0 +1,64 @@
+package org.hk.doghub.ui.components.shared;
+
+import com.vaadin.flow.component.textfield.TextField;
+import jakarta.validation.constraints.NotNull;
+import org.hk.doghub.model.NamedEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.vaadin.flow.component.icon.VaadinIcon.USER;
+import static com.vaadin.flow.data.value.ValueChangeMode.EAGER;
+import static java.text.MessageFormat.format;
+
+public class NameField extends TextField {
+
+    public static final String CLASS_NAME = "name-field";
+
+    public static final String LABEL = "Name";
+
+    public NameField() {
+        addClassName(CLASS_NAME);
+        setLabel(LABEL);
+        setRequiredIndicatorVisible(true);
+        setMinLength(2);
+        setMaxLength(128);
+        setPrefixComponent(USER.create());
+        setValueChangeMode(EAGER);
+        addValueChangeListener(this::valueChanged);
+    }
+
+    public void setValue(@NotNull NamedEntity entity) {
+        @NotNull String value = entity.getName();
+        setValue(value);
+    }
+
+    private void valueChanged(ComponentValueChangeEvent<TextField, String> event) {
+        @NotNull String value = event.getValue();
+        validateValue(value);
+    }
+
+    private void validateValue(String value) {
+        List<String> violations = validateField(value);
+        if(violations.isEmpty()) {
+            setInvalid(false);
+            setErrorMessage(null);
+        }
+        else {
+            setInvalid(true);
+            setErrorMessage(violations.get(0));
+        }
+    }
+
+    private List<String> validateField(String value) {
+        List<String> result = new ArrayList<>();
+        if(value == null || value.length() < 2 || value.length() > 128) {
+            result.add(format("{0} length must be between 2 and 128 characters", LABEL));
+        }
+        return result;
+    }
+
+    public List<String> validateField() {
+        return validateField(getValue());
+    }
+}
--- src/main/java/org/hk/doghub/ui/components/shared/tip/TipNameField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/tip/TipNameField.java
@@ -1,65 +1,12 @@
 package org.hk.doghub.ui.components.shared.tip;
 
-import com.vaadin.flow.component.textfield.TextField;
-import jakarta.validation.constraints.NotNull;
-import org.hk.doghub.model.NamedEntity;
+import org.hk.doghub.ui.components.shared.NameField;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.vaadin.flow.component.icon.VaadinIcon.LINK;
-import static com.vaadin.flow.data.value.ValueChangeMode.EAGER;
-import static java.text.MessageFormat.format;
-
-public class TipNameField extends TextField {
+public class TipNameField extends NameField {
 
     public static final String CLASS_NAME = "tip-name-field";
 
-    public static final String LABEL = "Name";
-
     public TipNameField() {
         addClassName(CLASS_NAME);
-        setLabel(LABEL);
-        setRequiredIndicatorVisible(true);
-        setReadOnly(true);
-        setMinLength(2);
-        setMaxLength(128);
-        setPrefixComponent(LINK.create());
-        setValueChangeMode(EAGER);
-        addValueChangeListener(this::valueChanged);
-    }
-
-    public void setValue(@NotNull NamedEntity entity) {
-        @NotNull String name = entity.getName();
-        setValue(name);
-    }
-
-    private void valueChanged(ComponentValueChangeEvent<TextField, String> event) {
-        @NotNull String value = event.getValue();
-        validateValue(value);
-    }
-
-    private void validateValue(@NotNull String value) {
-        List<String> violations = validateField(value);
-        if(violations.isEmpty()) {
-            setInvalid(false);
-            setErrorMessage(null);
-        }
-        else {
-            setInvalid(true);
-            setErrorMessage(violations.get(0));
-        }
-    }
-
-    private List<String> validateField(@NotNull String value) {
-        List<String> result = new ArrayList<>();
-        if(value.length() < 2 || value.length() > 128) {
-            result.add(format("{0} length must be between 2 and 128 characters", LABEL));
-        }
-        return result;
-    }
-
-    public List<String> validateField() {
-        return validateField(getValue());
     }
 }
