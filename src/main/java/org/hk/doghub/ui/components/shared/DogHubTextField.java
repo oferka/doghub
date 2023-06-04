94c745f29b327c56da698a9520b511c2fcf026ec
oferk <ofer.karp@gmail.com>
2023-06-04 17:04:43 +0300

    doghub 231

---
+++ src/main/java/org/hk/doghub/ui/components/shared/DogHubTextField.java
@@ -0,0 +1,58 @@
+package org.hk.doghub.ui.components.shared;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.textfield.TextField;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.vaadin.flow.data.value.ValueChangeMode.EAGER;
+import static java.text.MessageFormat.format;
+
+public class DogHubTextField extends TextField {
+
+    public static final String CLASS_NAME = "dog-hub-text-field";
+
+    public DogHubTextField(String label, boolean required, boolean readOnly, int minLength, int maxLength, Component prefixComponent, String allowedCharPattern) {
+        addClassName(CLASS_NAME);
+        setLabel(label);
+        setRequiredIndicatorVisible(required);
+        setReadOnly(readOnly);
+        setAllowedCharPattern(allowedCharPattern);
+        setMinLength(minLength);
+        setMaxLength(maxLength);
+        setPrefixComponent(prefixComponent);
+        setValueChangeMode(EAGER);
+        addValueChangeListener(this::valueChanged);
+    }
+
+    private void valueChanged(ComponentValueChangeEvent<TextField, String> event) {
+        @NotNull String value = event.getValue();
+        validateValue(value);
+    }
+
+    private void validateValue(@NotNull String value) {
+        List<String> violations = validateField(value);
+        if(violations.isEmpty()) {
+            setInvalid(false);
+            setErrorMessage(null);
+        }
+        else {
+            setInvalid(true);
+            setErrorMessage(violations.get(0));
+        }
+    }
+
+    private List<String> validateField(@NotNull String value) {
+        List<String> result = new ArrayList<>();
+        if(value.length() < getMinLength() || value.length() > getMaxLength()) {
+            result.add(format("{0} length must be between {1} and {2} characters", getLabel(), getMinLength(), getMaxLength()));
+        }
+        return result;
+    }
+
+    public List<String> validateField() {
+        return validateField(getValue());
+    }
+}
