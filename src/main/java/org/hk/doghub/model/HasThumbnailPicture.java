a8aef4ce5a77e741fbe06f6649b2dc0d2a9408be
oferk <ofer.karp@gmail.com>
2023-06-07 18:06:43 +0300

    doghub 320

--- src/main/java/org/hk/doghub/model/HasThumbnailPicture.java
+++ src/main/java/org/hk/doghub/model/HasThumbnailPicture.java
@@ -3,6 +3,7 @@ package org.hk.doghub.model;
 import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.URL;
 
+@FunctionalInterface
 public interface HasThumbnailPicture {
 
     int THUMBNAIL_PICTURE_MAX_LENGTH = 1024;
