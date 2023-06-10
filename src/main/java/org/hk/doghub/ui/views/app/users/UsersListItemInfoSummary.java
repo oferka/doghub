dd7109483f4f34f63077a7dbf130c65a9cdbc3a7
oferk <ofer.karp@gmail.com>
2023-06-10 17:12:49 +0300

    doghub 396

--- src/main/java/org/hk/doghub/ui/views/app/users/UsersListItemInfoSummary.java
+++ src/main/java/org/hk/doghub/ui/views/app/users/UsersListItemInfoSummary.java
@@ -1,16 +1,26 @@
 package org.hk.doghub.ui.views.app.users;
 
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.user.DogHubUser;
+import org.hk.doghub.ui.components.shared.EntitiesListItemInfoCreationTime;
 
-public class UsersListItemInfoSummary extends Span {
+public class UsersListItemInfoSummary extends VerticalLayout {
 
     public static final String CLASS_NAME = UsersListItemInfo.CLASS_NAME + "-summary";
 
+    private final EntitiesListItemInfoCreationTime<DogHubUser> creationDate;
+
+    private final Span summary;
+
     public UsersListItemInfoSummary(DogHubUser user) {
         addClassName(CLASS_NAME);
-        setText(getSummary(user));
+        creationDate = new EntitiesListItemInfoCreationTime<>(user);
+        add(creationDate);
+
+        summary = new Span(getSummary(user));
+        add(summary);
     }
 
     private @NotNull String getSummary(DogHubUser user) {
