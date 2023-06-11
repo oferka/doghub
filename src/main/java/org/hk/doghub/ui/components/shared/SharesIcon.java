4d669711923ae40d6b0f44eb00dcb87e55e7f080
oferk <ofer.karp@gmail.com>
2023-06-11 10:55:44 +0300

    doghub 412

--- src/main/java/org/hk/doghub/ui/components/shared/SharesIcon.java
+++ src/main/java/org/hk/doghub/ui/components/shared/SharesIcon.java
@@ -2,14 +2,14 @@ package org.hk.doghub.ui.components.shared;
 
 import com.vaadin.flow.component.icon.Icon;
 
-import static com.vaadin.flow.component.icon.VaadinIcon.SHARE;
+import static com.vaadin.flow.component.icon.VaadinIcon.SHARE_SQUARE;
 
 public class SharesIcon extends Icon {
 
     public static final String CLASS_NAME = "shares-icon";
 
     public SharesIcon() {
-        super(SHARE);
+        super(SHARE_SQUARE);
         addClassName(CLASS_NAME);
     }
 }
