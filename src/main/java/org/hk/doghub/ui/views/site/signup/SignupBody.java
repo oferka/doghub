0e607b8bb47d69b8a72bd68c8977f67586986906
oferk <ofer.karp@gmail.com>
2023-06-12 14:09:34 +0300

    doghub 472

--- src/main/java/org/hk/doghub/ui/views/site/signup/SignupBody.java
+++ src/main/java/org/hk/doghub/ui/views/site/signup/SignupBody.java
@@ -11,29 +11,13 @@ public class SignupBody extends VerticalLayout {
 
     public static final String CLASS_NAME = "signup-body";
 
-    private final SignupWith signupWith;
-
-    private final SignupDivider signupDivider;
-
-    private final UserCreationContainer userCreationContainer;
-
-    private final SignupMessage signupMessage;
-
     public SignupBody(AuthenticatedUser authenticatedUser, UserCreationService userCreationService) {
         addClassName(CLASS_NAME);
-
         setAlignItems(CENTER);
-
-        signupWith = new SignupWith();
-        add(signupWith);
-
-        signupDivider = new SignupDivider();
-        add(signupDivider);
-
-        userCreationContainer = new UserCreationContainer(authenticatedUser, userCreationService);
-        add(userCreationContainer);
-
-        signupMessage = new SignupMessage();
-        add(signupMessage);
+        SignupWith signupWith = new SignupWith();
+        SignupDivider signupDivider = new SignupDivider();
+        UserCreationContainer userCreationContainer = new UserCreationContainer(authenticatedUser, userCreationService);
+        SignupMessage signupMessage = new SignupMessage();
+        add(signupWith, signupDivider, userCreationContainer, signupMessage);
     }
 }
