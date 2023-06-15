839256778ad952ab025c6901716f866222c672f2
oferk <ofer.karp@gmail.com>
2023-06-15 16:24:52 +0300

    doghub 569

---
+++ src/test/java/org/hk/doghub/data/content/generator/tip/domcop/csv/CsvReaderTest.java
@@ -0,0 +1,22 @@
+package org.hk.doghub.data.content.generator.tip.domcop.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class CsvReaderTest {
+
+    @Autowired
+    private CsvReader reader;
+
+    @Test
+    void shouldGet() {
+        List<TipLine> tipLines = reader.read();
+        assertNotNull(tipLines);
+    }
+}
