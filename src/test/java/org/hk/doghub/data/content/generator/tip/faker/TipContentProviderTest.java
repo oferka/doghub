4699ebd73043c98308daeae86a7b45f7c4dde740
oferk <ofer.karp@gmail.com>
2023-06-15 15:30:16 +0300

    doghub 563

---
+++ src/test/java/org/hk/doghub/data/content/generator/tip/faker/TipContentProviderTest.java
@@ -0,0 +1,24 @@
+package org.hk.doghub.data.content.generator.tip.faker;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hk.doghub.data.content.generator.tip.Tip;
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+class TipContentProviderTest {
+
+    @Autowired
+    private TipContentProvider provider;
+
+    @RepeatedTest(10)
+    public void shouldGet() {
+        String value = provider.get(new Tip());
+        log.info(value);
+        assertNotNull(value);
+    }
+}
