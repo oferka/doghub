1c50cba3b30f1d0a1d74030ac830df95015f0dc4
oferk <ofer.karp@gmail.com>
2023-06-16 07:08:15 +0300

    doghub 574

---
+++ src/test/java/org/hk/doghub/data/content/generator/dog/faker/DogNameProviderTest.java
@@ -0,0 +1,24 @@
+package org.hk.doghub.data.content.generator.dog.faker;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hk.doghub.data.content.generator.dog.Dog;
+import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Slf4j
+class DogNameProviderTest {
+
+    @Autowired
+    private DogNameProvider provider;
+
+    @RepeatedTest(10)
+    public void shouldGet() {
+        String value = provider.get(new Dog());
+        log.info(value);
+        assertNotNull(value);
+    }
+}
