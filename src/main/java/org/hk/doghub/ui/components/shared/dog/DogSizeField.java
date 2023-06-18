d6bda0881a6dd2a63e79be73526c2c09f073b5c4
oferk <ofer.karp@gmail.com>
2023-06-18 08:13:53 +0300

    doghub 603

--- src/main/java/org/hk/doghub/ui/components/shared/dog/DogSizeField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/dog/DogSizeField.java
@@ -2,25 +2,26 @@ package org.hk.doghub.ui.components.shared.dog;
 
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.dog.DogHubDog;
-import org.hk.doghub.ui.components.shared.EditableTextField;
+import org.hk.doghub.ui.components.shared.DogHubComboBoxField;
 
-import static com.vaadin.flow.component.icon.VaadinIcon.TEXT_INPUT;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.hk.doghub.model.dog.DogHubDog.SIZE_MAX_LENGTH;
+import static org.vaadin.lineawesome.LineAwesomeIcon.COMPRESS_SOLID;
 
-public class DogSizeField extends EditableTextField {
+public class DogSizeField extends DogHubComboBoxField {
 
     public static final String CLASS_NAME = "dog-size-field";
 
     public static final String LABEL = "Size";
 
     public DogSizeField() {
-        super(LABEL, false, SIZE_MAX_LENGTH, TEXT_INPUT.create(), null);
+        super(LABEL, SIZE_MAX_LENGTH, COMPRESS_SOLID.create(), asList("small", "medium", "large", "extra large"));
         addClassName(CLASS_NAME);
     }
 
     public void setValue(@NotNull DogHubDog dog) {
-        String value = dog.getSize();
-        setValue((value != null) ? value : EMPTY);
+        String value = (dog.getSize() != null) ? dog.getSize() : EMPTY;
+        setValueWithValidation(value);
     }
 }
