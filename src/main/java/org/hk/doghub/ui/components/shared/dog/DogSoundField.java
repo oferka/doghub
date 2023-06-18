b1a895a8d64dde98c9d119e9ff96bece048fae1e
oferk <ofer.karp@gmail.com>
2023-06-18 07:48:30 +0300

    doghub 602

--- src/main/java/org/hk/doghub/ui/components/shared/dog/DogSoundField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/dog/DogSoundField.java
@@ -2,25 +2,26 @@ package org.hk.doghub.ui.components.shared.dog;
 
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.dog.DogHubDog;
-import org.hk.doghub.ui.components.shared.EditableTextField;
+import org.hk.doghub.ui.components.shared.DogHubComboBoxField;
 
-import static com.vaadin.flow.component.icon.VaadinIcon.TEXT_INPUT;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.hk.doghub.model.dog.DogHubDog.SOUND_MAX_LENGTH;
+import static org.vaadin.lineawesome.LineAwesomeIcon.MICROPHONE_SOLID;
 
-public class DogSoundField extends EditableTextField {
+public class DogSoundField extends DogHubComboBoxField {
 
     public static final String CLASS_NAME = "dog-sound-field";
 
     public static final String LABEL = "Sound";
 
     public DogSoundField() {
-        super(LABEL, false, SOUND_MAX_LENGTH, TEXT_INPUT.create(), null);
+        super(LABEL, SOUND_MAX_LENGTH, MICROPHONE_SOLID.create(), asList("woof", "woof woof", "bow wow", "ruff", "owooooo", "grrrrrr"));
         addClassName(CLASS_NAME);
     }
 
     public void setValue(@NotNull DogHubDog dog) {
-        String value = dog.getSound();
-        setValue((value != null) ? value : EMPTY);
+        String value = (dog.getSound() != null) ? dog.getSound() : EMPTY;
+        setValueWithValidation(value);
     }
 }
