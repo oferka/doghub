3ea3b7e86998e42b379985d727027f8f419cb711
oferk <ofer.karp@gmail.com>
2023-06-18 06:49:32 +0300

    doghub 600

--- src/main/java/org/hk/doghub/data/content/generator/dog/faker/DogBreedProvider.java
+++ src/main/java/org/hk/doghub/data/content/generator/dog/faker/DogBreedProvider.java
@@ -9,7 +9,6 @@ import org.hk.doghub.data.content.generator.dog.Dog;
 import org.springframework.stereotype.Service;
 
 import java.net.URI;
-import java.net.URISyntaxException;
 
 import static org.hk.doghub.data.content.generator.dog.Dog.BREED_MAX_LENGTH;
 
