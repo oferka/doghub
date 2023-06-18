13391ebc21faeb69eb54421c468785716c449eba
oferk <ofer.karp@gmail.com>
2023-06-18 07:00:31 +0300

    doghub 601

--- src/main/java/org/hk/doghub/ui/components/shared/dog/DogAgeField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/dog/DogAgeField.java
@@ -2,25 +2,26 @@ package org.hk.doghub.ui.components.shared.dog;
 
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.dog.DogHubDog;
-import org.hk.doghub.ui.components.shared.EditableTextField;
+import org.hk.doghub.ui.components.shared.DogHubComboBoxField;
 
-import static com.vaadin.flow.component.icon.VaadinIcon.TEXT_INPUT;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.hk.doghub.model.dog.DogHubDog.AGE_MAX_LENGTH;
+import static org.vaadin.lineawesome.LineAwesomeIcon.BABY_CARRIAGE_SOLID;
 
-public class DogAgeField extends EditableTextField {
+public class DogAgeField extends DogHubComboBoxField {
 
     public static final String CLASS_NAME = "dog-age-field";
 
     public static final String LABEL = "Age";
 
     public DogAgeField() {
-        super(LABEL, false, AGE_MAX_LENGTH, TEXT_INPUT.create(), null);
+        super(LABEL, AGE_MAX_LENGTH, BABY_CARRIAGE_SOLID.create(), asList("puppy", "young", "adult", "senior"));
         addClassName(CLASS_NAME);
     }
 
     public void setValue(@NotNull DogHubDog dog) {
-        String value = dog.getAge();
-        setValue((value != null) ? value : EMPTY);
+        String value = (dog.getAge() != null) ? dog.getAge() : EMPTY;
+        setValueWithValidation(value);
     }
 }
