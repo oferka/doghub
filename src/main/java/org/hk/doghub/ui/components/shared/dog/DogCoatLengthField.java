e7b9bfe2dfb43b7b69dfb2917566420ca53857b8
oferk <ofer.karp@gmail.com>
2023-06-18 08:18:25 +0300

    doghub 604

--- src/main/java/org/hk/doghub/ui/components/shared/dog/DogCoatLengthField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/dog/DogCoatLengthField.java
@@ -2,25 +2,26 @@ package org.hk.doghub.ui.components.shared.dog;
 
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.dog.DogHubDog;
-import org.hk.doghub.ui.components.shared.EditableTextField;
+import org.hk.doghub.ui.components.shared.DogHubComboBoxField;
 
-import static com.vaadin.flow.component.icon.VaadinIcon.TEXT_INPUT;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.hk.doghub.model.dog.DogHubDog.COAT_LENGTH_MAX_LENGTH;
+import static org.vaadin.lineawesome.LineAwesomeIcon.OTTER_SOLID;
 
-public class DogCoatLengthField extends EditableTextField {
+public class DogCoatLengthField extends DogHubComboBoxField {
 
     public static final String CLASS_NAME = "dog-coat-length-field";
 
     public static final String LABEL = "Coat Length";
 
     public DogCoatLengthField() {
-        super(LABEL, false, COAT_LENGTH_MAX_LENGTH, TEXT_INPUT.create(), null);
+        super(LABEL, COAT_LENGTH_MAX_LENGTH, OTTER_SOLID.create(), asList("hairless", "short", "medium", "long", "wire", "curly"));
         addClassName(CLASS_NAME);
     }
 
     public void setValue(@NotNull DogHubDog dog) {
-        String value = dog.getCoatLength();
-        setValue((value != null) ? value : EMPTY);
+        String value = (dog.getCoatLength() != null) ? dog.getCoatLength() : EMPTY;
+        setValueWithValidation(value);
     }
 }
