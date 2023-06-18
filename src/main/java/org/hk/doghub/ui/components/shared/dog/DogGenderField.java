a159cc0cc56fcf7828c6968cdd245005b2a470d4
oferk <ofer.karp@gmail.com>
2023-06-18 08:22:27 +0300

    doghub 605

--- src/main/java/org/hk/doghub/ui/components/shared/dog/DogGenderField.java
+++ src/main/java/org/hk/doghub/ui/components/shared/dog/DogGenderField.java
@@ -2,25 +2,26 @@ package org.hk.doghub.ui.components.shared.dog;
 
 import jakarta.validation.constraints.NotNull;
 import org.hk.doghub.model.dog.DogHubDog;
-import org.hk.doghub.ui.components.shared.EditableTextField;
+import org.hk.doghub.ui.components.shared.DogHubComboBoxField;
 
-import static com.vaadin.flow.component.icon.VaadinIcon.TEXT_INPUT;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.hk.doghub.model.dog.DogHubDog.GENDER_MAX_LENGTH;
+import static org.vaadin.lineawesome.LineAwesomeIcon.MERCURY_SOLID;
 
-public class DogGenderField extends EditableTextField {
+public class DogGenderField extends DogHubComboBoxField {
 
     public static final String CLASS_NAME = "dog-gender-field";
 
     public static final String LABEL = "Gender";
 
     public DogGenderField() {
-        super(LABEL, false, GENDER_MAX_LENGTH, TEXT_INPUT.create(), null);
+        super(LABEL, GENDER_MAX_LENGTH, MERCURY_SOLID.create(), asList("female", "male"));
         addClassName(CLASS_NAME);
     }
 
     public void setValue(@NotNull DogHubDog dog) {
-        String value = dog.getGender();
-        setValue((value != null) ? value : EMPTY);
+        String value = (dog.getGender() != null) ? dog.getGender() : EMPTY;
+        setValueWithValidation(value);
     }
 }
