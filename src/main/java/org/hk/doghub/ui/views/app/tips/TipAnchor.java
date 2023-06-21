19549077497947ccab4da9cd9d1b90fb53f57eb5
oferk <ofer.karp@gmail.com>
2023-06-21 11:11:34 +0300

    doghub 645

--- src/main/java/org/hk/doghub/ui/views/app/tips/TipAnchor.java
+++ src/main/java/org/hk/doghub/ui/views/app/tips/TipAnchor.java
@@ -1,13 +1,13 @@
 package org.hk.doghub.ui.views.app.tips;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.shared.Tooltip;
 import org.hk.doghub.model.tip.DogHubTip;
 
 import static com.vaadin.flow.component.html.AnchorTarget.BLANK;
-import static com.vaadin.flow.component.icon.VaadinIcon.EXTERNAL_LINK;
 import static java.lang.String.format;
+import static org.vaadin.lineawesome.LineAwesomeIcon.EXTERNAL_LINK_ALT_SOLID;
 
 public class TipAnchor extends Anchor {
 
@@ -18,7 +18,7 @@ public class TipAnchor extends Anchor {
         String moreInfo = tip.getMoreInfo();
         if(moreInfo != null) {
             setHref(format("https://www.%s", tip.getMoreInfo()));
-            Icon externalLinkIcon = new Icon(EXTERNAL_LINK);
+            Component externalLinkIcon = EXTERNAL_LINK_ALT_SOLID.create();
             externalLinkIcon.addClassName(format("%s-icon", CLASS_NAME));
             Tooltip.forComponent(externalLinkIcon).setText("Open link in a new tab");
             add(externalLinkIcon);
