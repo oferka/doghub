25aeffc882283af4b628b837278a8130ebb00a3b
oferk <ofer.karp@gmail.com>
2023-06-22 10:38:13 +0300

    doghub 649

--- src/test/java/org/hk/doghub/ui/e2e/BaseLandingPageTest.java
+++ src/test/java/org/hk/doghub/ui/e2e/BaseLandingPageTest.java
@@ -1,15 +1,15 @@
 package org.hk.doghub.ui.e2e;
 
+import org.hk.doghub.DogHubApplication;
 import org.hk.doghub.automation.e2e.selenium.browser.BrowserProvider;
 import org.hk.doghub.automation.e2e.selenium.browser.WebDriverProvider;
 import org.hk.doghub.ui.automation.DogHubLandingPageService;
-import org.hk.doghub.ui.automation.DogHubTestApplication;
 import org.junit.jupiter.api.AfterEach;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DogHubTestApplication.class)
+@SpringBootTest(classes = DogHubApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public abstract class BaseLandingPageTest {
 
     @Autowired
