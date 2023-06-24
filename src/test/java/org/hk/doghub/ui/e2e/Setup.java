677dcbf2be2b0a8f79048ec09af81e7e688f55dd
oferk <ofer.karp@gmail.com>
2023-06-24 18:05:58 +0300

    doghub 685

--- src/test/java/org/hk/doghub/ui/e2e/Setup.java
+++ src/test/java/org/hk/doghub/ui/e2e/Setup.java
@@ -17,13 +17,13 @@ public enum Setup {
 //    SETUP_2("setup 2", new BrowserConfiguration(Browser.BrowserType.CHROME, true, true)),
 //    SETUP_3("setup 3", new BrowserConfiguration(Browser.BrowserType.CHROME, false, false)),
 //    SETUP_4("setup 4", new BrowserConfiguration(Browser.BrowserType.CHROME, false, true)),
-    SETUP_5("setup 5", new BrowserConfiguration(Browser.BrowserType.FIREFOX, true, false));
+    SETUP_5("setup 5", new BrowserConfiguration(Browser.BrowserType.FIREFOX, true, false)),
 //    SETUP_6("setup 6", new BrowserConfiguration(Browser.BrowserType.FIREFOX, true, true)),
 //    SETUP_7("setup 7", new BrowserConfiguration(Browser.BrowserType.FIREFOX, false, false)),
 //    SETUP_8("setup 8", new BrowserConfiguration(Browser.BrowserType.FIREFOX, false, true)),
-//    SETUP_9("setup 9", new BrowserConfiguration(Browser.BrowserType.EDGE, true, true)),
+//    SETUP_9("setup 9", new BrowserConfiguration(Browser.BrowserType.EDGE, true, false)),
 //    SETUP_10("setup 10", new BrowserConfiguration(Browser.BrowserType.EDGE, false, true)),
-//    SETUP_11("setup 11", new BrowserConfiguration(Browser.BrowserType.SAFARI, true, false)),
+    SETUP_11("setup 11", new BrowserConfiguration(Browser.BrowserType.SAFARI, true, false));
 //    SETUP_12("setup 12", new BrowserConfiguration(Browser.BrowserType.SAFARI, false, false));
 
     @Getter
