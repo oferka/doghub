3e6c56700221b464c769ecb75d60f8b597e4bb5a
oferk <ofer.karp@gmail.com>
2023-06-27 11:56:16 +0300

    doghub 712

--- src/main/java/org/hk/doghub/data/content/provider/user/AdminDogHubUserProvider.java
+++ src/main/java/org/hk/doghub/data/content/provider/user/AdminDogHubUserProvider.java
@@ -9,10 +9,6 @@ import org.hk.doghub.model.user.DogHubUser;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Set;
 
 import static org.hk.doghub.model.user.Role.ADMIN;
@@ -33,14 +29,14 @@ public class AdminDogHubUserProvider {
         adminUsaUser.setName(configuration.getName());
         adminUsaUser.setUsername(configuration.getUsername());
         adminUsaUser.setEmail(configuration.getEmail());
-        adminUsaUser.setTitle("Mr.");
-        adminUsaUser.setThumbnailPicture("https://en.gravatar.com/userimage/33131799/3024d9952717995b6facc57f7bd50d2e.png");
-        adminUsaUser.setDateOfBirth(ZonedDateTime.of(LocalDate.of(1976, 4, 15), LocalTime.of(2,  44), ZoneId.of("GMT+02:00")));
-        adminUsaUser.setCreationTime(ZonedDateTime.of(LocalDate.of(2022, 3, 19), LocalTime.of(6,  35), ZoneId.of("GMT+02:00")));
-        adminUsaUser.setCompany("Karpisimo.com");
+        adminUsaUser.setTitle(configuration.getTitle());
+        adminUsaUser.setThumbnailPicture(configuration.getThumbnailPicture());
+        adminUsaUser.setDateOfBirth(configuration.getDateOfBirth());
+        adminUsaUser.setCreationTime(configuration.getCreationTime());
+        adminUsaUser.setCompany(configuration.getCompany());
         adminUsaUser.setAddress(getAddress());
-        adminUsaUser.setMobileNumber("+972545597059");
-        adminUsaUser.setHashedPassword(passwordEncoder.encode("password"));
+        adminUsaUser.setMobileNumber(configuration.getMobileNumber());
+        adminUsaUser.setHashedPassword(passwordEncoder.encode(configuration.getPassword()));
         adminUsaUser.setFeedback(getFeedback());
         adminUsaUser.setRoles(Set.of(USER, ADMIN));
         return adminUsaUser;
@@ -48,20 +44,20 @@ public class AdminDogHubUserProvider {
 
     private @NotNull DogHubAddress getAddress() {
         DogHubAddress address = new DogHubAddress();
-        address.setNumber(22);
-        address.setStreetName("Holy Tree");
-        address.setCity("Shadmot Dvora");
-        address.setState("Lower Galilee");
-        address.setCountry("Israel");
-        address.setPostcode("1524000");
+        address.setNumber(configuration.getNumber());
+        address.setStreetName(configuration.getStreetName());
+        address.setCity(configuration.getCity());
+        address.setState(configuration.getState());
+        address.setCountry(configuration.getCountry());
+        address.setPostcode(configuration.getPostcode());
         return address;
     }
 
     private @NotNull DogHubFeedback getFeedback() {
         DogHubFeedback result = new DogHubFeedback();
-        result.setLikes(12L);
-        result.setComments(11L);
-        result.setShares(10L);
+        result.setLikes(configuration.getLikes());
+        result.setComments(configuration.getComments());
+        result.setShares(configuration.getShares());
         return result;
     }
 }
--- src/main/java/org/hk/doghub/data/content/provider/user/AdminDogHubUserProviderConfiguration.java
+++ src/main/java/org/hk/doghub/data/content/provider/user/AdminDogHubUserProviderConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import java.time.ZonedDateTime;
+
 @Configuration
 @PropertySource("classpath:admin-user.properties")
 @ConfigurationProperties(prefix = "admin.user")
@@ -14,4 +16,20 @@ public class AdminDogHubUserProviderConfiguration {
     private String name;
     private String username;
     private String email;
+    private String title;
+    private String thumbnailPicture;
+    private ZonedDateTime dateOfBirth;
+    private ZonedDateTime creationTime;
+    private String company;
+    private String mobileNumber;
+    private String password;
+    private int number;
+    private String streetName;
+    private String city;
+    private String state;
+    private String country;
+    private String postcode;
+    private long likes;
+    private long shares;
+    private long comments;
 }
