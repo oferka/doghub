cf1246d5544c9d6e20549c27d04a4268e607ef7b
oferk <ofer.karp@gmail.com>
2023-06-30 08:54:08 +0300

    doghub 740

--- src/main/java/org/hk/doghub/ui/components/shared/user/SignOutButton.java
+++ src/main/java/org/hk/doghub/ui/components/shared/user/SignOutButton.java
@@ -3,12 +3,15 @@ package org.hk.doghub.ui.components.shared.user;
 import com.vaadin.flow.component.button.Button;
 import org.hk.doghub.security.AuthenticatedUser;
 
+import static org.vaadin.lineawesome.LineAwesomeIcon.SIGN_OUT_ALT_SOLID;
+
 public class SignOutButton extends Button {
 
     public static final String CLASS_NAME = "sign-out-button";
 
     public SignOutButton(AuthenticatedUser authenticatedUser) {
         addClassName(CLASS_NAME);
+        setIcon(SIGN_OUT_ALT_SOLID.create());
         setText("Sign out");
         addClickListener(e -> authenticatedUser.logout());
     }
