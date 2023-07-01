f6d1cf28912d2205aba9a25b340dc8b52022b83c
oferk <ofer.karp@gmail.com>
2023-07-01 09:55:58 +0300

    doghub 748

--- src/main/java/org/hk/doghub/ui/automation/ProfileViewService.java
+++ src/main/java/org/hk/doghub/ui/automation/ProfileViewService.java
@@ -4,10 +4,15 @@ import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hk.doghub.automation.e2e.selenium.element.display.status.ElementDisplayStatusRetriever;
-import org.hk.doghub.ui.components.shared.IdField;
+import org.hk.doghub.automation.e2e.selenium.element.retrieve.ElementRetriever;
+import org.hk.doghub.ui.components.shared.user.UserIdField;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Service;
+
+import static com.vaadin.flow.component.Tag.INPUT;
+import static java.text.MessageFormat.format;
 
 @Service
 @RequiredArgsConstructor
@@ -18,11 +23,17 @@ public class ProfileViewService {
 
     private final ElementDisplayStatusRetriever elementDisplayStatusRetriever;
 
+    private final ElementRetriever elementRetriever;
+
     public void navigateFromHomePage(@NotNull WebDriver webDriver) {
         userMenuBarService.navigateToProfileView(webDriver);
     }
 
     public void verifyIdDisplayed(@NotNull WebDriver webDriver) {
-        elementDisplayStatusRetriever.isDisplayed(webDriver, By.className(IdField.CLASS_NAME), true);
+        By fieldLocator = By.className(UserIdField.CLASS_NAME);
+        assert elementDisplayStatusRetriever.isDisplayed(webDriver, fieldLocator);
+        WebElement inputElement = elementRetriever.getByPresence(webDriver, fieldLocator).findElement(By.tagName(INPUT));
+        String value = inputElement.getAttribute("value");
+        log.info(format("Field value is: {0}", value));
     }
 }
