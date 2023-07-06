6781dc82e24c514526c89bf1ac95008c18a27b97
oferk <ofer.karp@gmail.com>
2023-07-06 09:55:36 +0300

    doghub 794

--- src/main/java/org/hk/doghub/data/repository/user/DogHubUserRepository.java
+++ src/main/java/org/hk/doghub/data/repository/user/DogHubUserRepository.java
@@ -4,13 +4,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hk.doghub.data.repository.EntityRepository;
 import org.hk.doghub.model.user.DogHubUser;
-import org.springframework.validation.annotation.Validated;
 
 import java.util.Optional;
 
 import static org.hk.doghub.model.user.DogHubUser.USER_NAME_MAX_LENGTH;
 
-@Validated
 public interface DogHubUserRepository extends EntityRepository<DogHubUser> {
 
     Optional<DogHubUser> findByUsername(@NotNull @Size(max = USER_NAME_MAX_LENGTH) String username);
