63d82050841341914d64f549331a788949eb9df9
oferk <ofer.karp@gmail.com>
2023-07-06 12:05:45 +0300

    doghub 797

--- src/test/java/org/hk/doghub/data/repository/user/DogHubUserRepositoryTest.java
+++ src/test/java/org/hk/doghub/data/repository/user/DogHubUserRepositoryTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.hk.doghub.model.user.DogHubUser;
 import org.junit.jupiter.api.RepeatedTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.transaction.TransactionSystemException;
 
 import java.time.Duration;
@@ -11,6 +12,7 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 import static org.hk.doghub.model.NamedEntity.NAME_MAX_LENGTH;
+import static org.hk.doghub.model.user.DogHubUser.USER_NAME_MAX_LENGTH;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DogHubUserRepositoryTest extends DogHubUserDataTest {
@@ -69,5 +71,30 @@ class DogHubUserRepositoryTest extends DogHubUserDataTest {
         DogHubUser item = dogHubUserProvider.get();
         item.setName(RandomStringUtils.randomAlphabetic(NAME_MAX_LENGTH+1));
         assertThrows(TransactionSystemException.class, () -> dogHubUserRepository.save(item));
+    }
+
+    @RepeatedTest(10)
+    void shouldNotSaveUserWithNullUserName() {
+        DogHubUser item = dogHubUserProvider.get();
+        item.setUsername(null);
+        assertThrows(TransactionSystemException.class, () -> dogHubUserRepository.save(item));
+    }
+
+    @RepeatedTest(10)
+    void shouldNotSaveUserWithUsernameThatExceedsMaxLength() {
+        DogHubUser item = dogHubUserProvider.get();
+        item.setUsername(RandomStringUtils.randomAlphabetic(USER_NAME_MAX_LENGTH+1));
+        assertThrows(TransactionSystemException.class, () -> dogHubUserRepository.save(item));
+    }
+
+    @RepeatedTest(10)
+    void shouldNotSaveUserWithNonUniqueUserName() {
+        DogHubUser item1 = dogHubUserProvider.get();
+        DogHubUser saved = dogHubUserRepository.save(item1);
+        assertEquals(item1.getUsername(), saved.getUsername());
+        DogHubUser item2 = dogHubUserProvider.get();
+        item2.setUsername(item1.getUsername());
+        assertThrows(DataIntegrityViolationException.class, () -> dogHubUserRepository.save(item2));
+        dogHubUserRepository.delete(saved);
     }
 }
