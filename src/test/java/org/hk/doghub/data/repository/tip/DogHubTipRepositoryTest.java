adab3a10ed4e27d10ee64fb9234032588d9de698
oferk <ofer.karp@gmail.com>
2023-07-08 16:08:50 +0300

    doghub 854

--- src/test/java/org/hk/doghub/data/repository/tip/DogHubTipRepositoryTest.java
+++ src/test/java/org/hk/doghub/data/repository/tip/DogHubTipRepositoryTest.java
@@ -3,6 +3,8 @@ package org.hk.doghub.data.repository.tip;
 import org.hk.doghub.model.tip.DogHubTip;
 import org.junit.jupiter.api.RepeatedTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DogHubTipRepositoryTest extends DogHubTipDataTest {
@@ -13,5 +15,13 @@ class DogHubTipRepositoryTest extends DogHubTipDataTest {
         DogHubTip saved = dogHubTipRepository.save(item);
         assertEquals(item.getTitle(), saved.getTitle());
         dogHubTipRepository.delete(saved);
+    }
+
+    @RepeatedTest(10)
+    void shouldSaveAllTips() {
+        List<DogHubTip> items = dogHubTipProvider.get(getNumberOfItemsToLoad());
+        List<DogHubTip> saved = dogHubTipRepository.saveAll(items);
+        assertEquals(saved.size(), items.size());
+        dogHubTipRepository.deleteAll(saved);
     }
 }
