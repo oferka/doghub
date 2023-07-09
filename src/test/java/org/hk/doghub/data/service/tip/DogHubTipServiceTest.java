810c4ca577b9d3e5ee3fb991b2797e11d0d4f93d
oferk <ofer.karp@gmail.com>
2023-07-09 09:53:26 +0300

    doghub 918

--- src/test/java/org/hk/doghub/data/service/tip/DogHubTipServiceTest.java
+++ src/test/java/org/hk/doghub/data/service/tip/DogHubTipServiceTest.java
@@ -186,4 +186,16 @@ class DogHubTipServiceTest extends DogHubTipDataTest {
         dogHubTipRepository.delete(saved1);
         dogHubTipRepository.delete(saved2);
     }
+
+    @RepeatedTest(10)
+    void shouldFindNextTipByCreatedBy() {
+        DogHubTip item1 = dogHubTipProvider.get();
+        DogHubTip saved1 = dogHubTipRepository.save(item1);
+        DogHubTip item2 = dogHubTipProvider.get();
+        item2.setCreatedBy(saved1.getCreatedBy());
+        DogHubTip saved2 = dogHubTipRepository.save(item2);
+        assertTrue(dogHubTipService.findNextByCreatedBy(saved1.getId(), saved1.getCreatedBy()).isPresent());
+        dogHubTipRepository.delete(saved1);
+        dogHubTipRepository.delete(saved2);
+    }
 }
